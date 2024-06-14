e1a315c350cefb72404d1325c6531ac87f191f65
tigaron <suputra@mail.com>
2024-06-14 13:45:29 +0700

    fix: image and skeleton rendered after the other

--- app/(main)/(routes)/webtoons/[provider]/[webtoon]/[chapter]/_components/chapter-image.tsx
+++ app/(main)/(routes)/webtoons/[provider]/[webtoon]/[chapter]/_components/chapter-image.tsx
@@ -2,6 +2,9 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { toast } from "sonner";
+
+import { ImageSkeleton } from "./image-skeleton";
 
 interface ChapterImageProps {
   contentURL: string;
@@ -13,39 +16,29 @@ export default function ChapterImage({
   fullTitle,
 }: ChapterImageProps) {
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
+  const handleImageError = () => {
+    setLoading(false);
+    toast.error("Failed to load image");
+  };
 
   return (
     <div className="mx-auto w-full">
-      {loading && (
-        <div className="mx-auto mb-2 w-full animate-pulse">
-          <div className="flex h-48 w-full items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
-            <svg
-              className="h-12 w-12 text-gray-200"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 640 512"
-            >
-              <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-            </svg>
-          </div>
-          <span className="sr-only">Loading...</span>
-        </div>
-      )}
-      {error && <p className="text-center">Failed to load image</p>}
+      {loading && <ImageSkeleton />}
       <Image
-        onLoadingComplete={() => setLoading(false)}
-        onError={() => {
-          setLoading(false);
-          setError(true);
-        }}
         src={contentURL}
         alt={fullTitle}
-        className="h-auto w-full"
+        className={`h-auto w-full ${loading ? 'invisible' : 'visible'}`}
         sizes="100vw"
         height={0}
         width={0}
         unoptimized
+        onLoad={handleImageLoad}
+        onError={handleImageError}
       />
     </div>
   );
